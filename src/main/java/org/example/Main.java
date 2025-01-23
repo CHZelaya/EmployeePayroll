0d4449c9955e18c8b7075ae92379c8d20eb556e7
CHZelaya <chzelaya1201@gmail.com>
2025-01-22 22:00:32 -0700

    Employee class complete, successfully grabbing user inputs name and hourlyRate with input validation in place

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("*******************STARTING PAYROLL APPLICATION******************\n\n");
+
+
+        //Create Scanner Object for user input
+        Scanner scanner = new Scanner(System.in);
+        // Get NAME and HOURLY RATE
+        String name = Employee.getName(scanner);
+        double hourlyRate = Employee.getHourlyRate(scanner);
+
+        // Create an object of Employee with input from user
+        Employee employee = new Employee(name, hourlyRate);
+            // Print to test
+            System.out.println(employee.toString());
     }
-}
+
+
+
+
+}// class
